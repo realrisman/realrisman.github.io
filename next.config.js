af291348c5ff19389d6dbacb2294e1f1a2c013fd
Muhamad Risman <real.risman@gmail.com>
2023-06-12 06:27:04 +0700

    chore: add webpack config for load svg files as React component

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  webpack(config) {
+    // Import `svg` files as React components
+    config.module.rules.push({
+      test: /\.svg$/,
+      resourceQuery: { not: [/url/] },
+      use: [{ loader: "@svgr/webpack", options: { svgo: false } }],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;

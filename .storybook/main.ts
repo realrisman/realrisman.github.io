e5df8a025121724a9dafc9a104e2ded358623477
Muhamad Risman <real.risman@gmail.com>
2023-06-12 06:27:34 +0700

    chore: fix storybook config to load stories and svg files

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,44 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+
 const config: StorybookConfig = {
-  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../app/**/*.stories.mdx", "../app/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
+  core: {
+    builder: {
+      name: "@storybook/builder-webpack5",
+      options: {
+        fsCache: true,
+        lazyCompilation: true,
+      },
+    },
+  },
+  webpackFinal: (config) => {
+    const rules = config?.module?.rules || [];
+
+    const imageRule = rules.find((rule) => {
+      const test = (rule as { test: RegExp }).test;
+
+      if (!test) {
+        return false;
+      }
+
+      return test.test(".svg");
+    }) as { [key: string]: any };
+
+    imageRule.exclude = /\.svg$/;
+
+    rules.unshift({
+      test: /\.svg$/,
+      resourceQuery: { not: [/url/] },
+      use: [{ loader: "@svgr/webpack", options: { svgo: false } }],
+    });
+
+    return config;
+  },
   framework: {
     name: "@storybook/nextjs",
     options: {},

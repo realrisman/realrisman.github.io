a2b13bcaf8c39d8a8eb791fe4a69cc3ddea1d03d
Muhamad Risman <real.risman@gmail.com>
2023-06-13 07:36:40 +0700

    fix: visually hidden styles

--- app/components/visually-hidden/visually-hidden.tsx
+++ app/components/visually-hidden/visually-hidden.tsx
@@ -1,4 +1,7 @@
+import { classes } from "@/app/utils/style";
 import { forwardRef } from "react";
+
+import styles from "./visually-hidden.module.css";
 
 interface VisuallyHiddenProps {
   className?: string;
@@ -21,7 +24,7 @@ export const VisuallyHidden = forwardRef(function VisuallyHidden(
 ) {
   return (
     <Component
-      className={`absolute ${className}`}
+      className={classes(styles.hidden, className)}
       data-hidden={!visible && !showOnFocus}
       data-show-on-focus={showOnFocus}
       ref={ref}
